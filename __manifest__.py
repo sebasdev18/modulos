b23ab687b9ce028c187ea91b64b3e05631ffc296
sebasdev18 <115429993+sebasdev18@users.noreply.github.com>
2023-06-19 18:22:45 -0300

    mi primer modulo

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+'name': "Mi primer modulo",
+'summary': "Mi primer modulo de Odoo",
+'description': """Este modulo servira de aprendizaje para poder dedicarme a lo que me gusta lo antes posible""",
+'author': "Seba",
+'license': "AGPL-3",
+'website': "Proximamente...",
+'category': 'Uncategorized',
+'version': '1.0',
+'depends': ['base'],
+'application': True,
+'data': ['views/views.xml'],
+}
